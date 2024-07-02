5d996c08d661e8d0161f205eae7973f4e41f8ed2
Norlan1223 <isaiaslargaespada@gmail.com>
2024-07-01 23:35:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si mi luz')
+    alert('fue una sabia repuesta yo te apoyo jaja')
 });
 
 const noBtn = document.querySelector('#noBtn');
